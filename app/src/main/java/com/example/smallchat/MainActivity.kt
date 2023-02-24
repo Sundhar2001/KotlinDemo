37ed602c8e5f4bad94b9249bc867a4017311ecf0
Sundhar <sundharamoorthi2004@gmail.com>
2023-02-24 20:21:29 +0530

    1.Get message from user and validate the edit text. 2.Move one activity to another activity (Explicit).

--- app/src/main/java/com/example/smallchat/MainActivity.kt
+++ app/src/main/java/com/example/smallchat/MainActivity.kt
@@ -1,26 +1,55 @@
 package com.example.smallchat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var btnShowToast: Button
+   lateinit var btnShowToast: Button
+   lateinit var userMessage: EditText
+   lateinit var btnSendMessage: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*hide default toolBar in layout file*/
         supportActionBar?.hide()
 
-         btnShowToast = findViewById(R.id.btnShowToast)
+         btnShowToast = findViewById(R.id.btn_show_toast)
+         btnSendMessage = findViewById(R.id.btn_send_message)
+         userMessage = findViewById(R.id.et_user_message)
 
         btnShowToast.setOnClickListener {
             Log.i("MainActivity","Button was clicked !")
             Toast.makeText(this,"Welcome",Toast.LENGTH_LONG).show()
+        }
+
+        btnSendMessage.setOnClickListener {
+
+            var message = userMessage.text.toString()    /*val message = userMessage.text.toString() ----> //possible// */
+
+            /*validate textview is empty or not*/
+            if (message.isEmpty()){
+                Log.d("IntentButton","ErrorMessage")
+                Toast.makeText(this,"Please enter the value",Toast.LENGTH_SHORT).show()
+
+            }else{
+
+                /*Explicit intent --->  current (this) activity to another(ReceiverActivity) activity*/
+                val  messageIntent = Intent(this,ReceiverActivity::class.java)
+                startActivity(messageIntent)
+                Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+
+                Log.d("IntentButton","SuccessMessage")
+
+            }
+
         }
     }
 }
---
+++ app/src/main/java/com/example/smallchat/ReceiverActivity.kt
@@ -0,0 +1,12 @@
+package com.example.smallchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReceiverActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receiver)
+    }
+}
